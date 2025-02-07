2f528f12dae8fe4f5767922a3271ce2d8029c1c6
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-02-07 23:04:53 +0530

    ArrayExamples for section 17

---
+++ Section 17 Arrays/arrayExample.js
@@ -0,0 +1,16 @@
+//Arrays in java Script are a single object that can hold multiple values.
+//The values can be of any type, including other arrays.
+
+//Code for arrayExample.js:   
+// Making an array:
+const colors = ["red", "orange", "yellow"];
+
+// Arrays are indexed like strings:
+console.log(colors[0]); // "red"
+
+// They have a length:
+console.log(colors.length); //3
+
+// Important array methods:
+
+//push(value) - adds value to the END of an array
--- Section 17 Arrays/array_cheatsheet.js
+++ Section 17 Arrays/array_cheatsheet.js
@@ -5,7 +5,7 @@ const colors = ["red", "orange", "yellow"];
 colors[0]; // "red"
 
 // They have a length:
-colors.length; //3
+//colors.length; //3
 
 // Important array methods:
 //push(value) - adds value to the END of an array
@@ -13,3 +13,13 @@ colors.length; //3
 
 //unshift(val) - adds value to START of an array
 //shift() - removes and returns first element in an array
+
+//concat(arr) - returns a NEW array with the values of arr combined with the original array
+//includes(val) - returns a boolean if the value is in the array
+//indexOf(val) - returns the first index of a value in an array, -1 if not found
+//join(char) - returns a string of all elements joined by the parameter
+//reverse() - reverses the array
+//slice(startIdx, endIdx) - returns a new array from startIdx to endIdx
+
+Array.isArray(colors); // true
+colors.push("green"); // ["red", "orange", "yellow", "green"]
